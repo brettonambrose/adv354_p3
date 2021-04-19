ac28a4ef97821d80634961b27ebe25e9b03b5f1c
Bretton Fry <Bretton.fry@gmail.com>
2021-04-19 19:17:55 -0400

    Add files via upload

--- php/contact_submit.php
+++ php/contact_submit.php
@@ -1,5 +1,5 @@
-//Brett wrote this mailer
 <?php
+// Brett wrote this mailer
     $sender = $_POST['yourname'];
     $reply = $_POST['youremail'];
     $refer = $_POST['refertype'];
@@ -11,5 +11,6 @@
 
     mail($recipient, $subject, $message, $mailhead)
         or die("Failed.");
-    Echo "Thank you for contacting us!";
+    echo "<b>Thank you for contacting us!<br>You will be redirected back to the contact page in 5 seconds.<br>If redirection does not work, click <a href='../contact.html'>here</a>.</b>";
+    header('refresh: 5; url=../contact.html'); // Back to contact page after 5 seconds
 ?>
